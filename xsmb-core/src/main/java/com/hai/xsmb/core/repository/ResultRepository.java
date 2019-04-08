bbade8d952160ecc4af7d8322472b6bbcaa36f47
Hai HN <hoang.ngoc.hai@sun-asterisk.com>
2019-04-08 13:37:17 +0700

    Result API

--- xsmb-core/src/main/java/com/hai/xsmb/core/repository/ResultRepository.java
+++ xsmb-core/src/main/java/com/hai/xsmb/core/repository/ResultRepository.java
@@ -4,7 +4,9 @@ import com.hai.xsmb.core.entity.ResultEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository
 public interface ResultRepository extends JpaRepository<ResultEntity, Long> {
-
+    ResultEntity getByDateAndDeleted(Date date, Boolean deleted);
 }
